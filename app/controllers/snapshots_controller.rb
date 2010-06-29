558cce619ed30eeee910838db868747d476a94dc
Christoph <christoph@zerodeviation.net>
2010-06-29 17:34:11 -0500

    initial import

---
+++ app/controllers/snapshots_controller.rb
@@ -0,0 +1,32 @@
+# redMine - project management software
+# Copyright (C) 2006-2007  Jean-Philippe Lang
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+class SnapshotsController < ApplicationController
+  layout 'base'
+  before_filter :find_project, :authorize
+  
+  def show  
+    @files = Dir.glob("public/files/projects/#{@project.identifier}/*")
+  end
+  
+private
+  def find_project   
+    @project = Project.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render_404
+  end
+end
